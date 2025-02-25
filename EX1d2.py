cd7a3462486fa650ef228811ffd7aa7477d084e4
saisuma1906 <saisumachandana9@gmail.com>
2025-02-25 14:34:38 -0500

    Update EX1d2.py

--- EX1d2.py
+++ EX1d2.py
@@ -27,12 +27,13 @@ with col1:
 with col2:
   st.metric("Total Orders", len(df))
 col1,col2=st.columns(2)
-with col1
+
+with col1:
 # Data Table
 st.subheader("Sales Data")
 st.dataframe(df)
 
-with col2
+with col2:
 # Chart
 st.subheader("Sales by Category")
 category_sales = df.groupby('Category')['Total'].sum().reset_index()
